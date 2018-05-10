3f91d6b8f0c7f08b372f998da7c7ea16463dd732
Adam Joyce <adamjoyce@outlook.com>
2018-05-10 11:26:33 +0100

    Project + webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, use: 'babel-loader'},
+      {test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader']}
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
+};
